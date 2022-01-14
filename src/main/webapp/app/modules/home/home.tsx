17cfd1cad9454c991c6c1d7632ca3805864c4489
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2022-01-14 03:23:09 -0500

    removed login requirement on home page

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -28,8 +28,6 @@ function posterSearch() {
     getPoster();
   };
 
-  const account = useAppSelector(state => state.authentication.account);
-
   return (
     <Row>
       <Col md="3" className="pad"></Col>
@@ -40,89 +38,65 @@ function posterSearch() {
         <p className="lead">
           <Translate contentKey="home.subtitle"></Translate>
         </p>
-        {account?.login ? (
-          <div>
-            <Col md="5">
-              <div className="app">
-                <form className="app__searchForm" onSubmit={onSubmit}>
-                  <input
-                    type="text"
-                    className="app__input"
-                    placeholder="Find Videos"
-                    value={query}
-                    onChange={e => setquery(e.target.value)}
-                  />
-                  <input className="app__submit" type="submit" value="Search" />
-                </form>
-              </div>
-              <div className="app__videos">
-                {videos.map(video => {
-                  const value = video.id;
-                  const url = `https://api.themoviedb.org/3/movie/${value}/videos?api_key=616093e66ab252685ad921e5c4680152`;
-                  var videoDisplay;
-                  fetch(url)
-                    .then(res => res.json())
-                    .then(data => (videoDisplay = data))
-                    .then(() => console.log(videoDisplay));
-                  return (
-                    <div
-                      key={video.id}
-                      className="VideoTile"
-                      onClick={() => {
-                        {
-                          video.poster_path == null
-                            ? window.open(`https://www.youtube.com/results?search_query=${query}`)
-                            : window.open(`https://www.youtube.com/watch?v=${videoDisplay.results[0].key}`);
-                        }
-                      }}
-                    >
-                      <div className="row">
-                        {video.poster_path == null ? (
-                          <img
-                            className="videoTile__img"
-                            src={`https://c.tenor.com/0bN9L54PMmsAAAAC/coming-soon-see-it-soon.gif`}
-                            alt="card image"
-                            style={{ width: '100%', height: 360 }}
-                          />
-                        ) : (
-                          <img
-                            className="videoTile__img"
-                            src={`https://image.tmdb.org/t/p/w185${video.poster_path}`}
-                            alt="card image"
-                            style={{ width: '100%', height: 360 }}
-                          />
-                        )}
-                      </div>
+        <div>
+          <Col md="5">
+            <div className="app">
+              <form className="app__searchForm" onSubmit={onSubmit}>
+                <input
+                  type="text"
+                  className="app__input"
+                  placeholder="Find Videos"
+                  value={query}
+                  onChange={e => setquery(e.target.value)}
+                />
+                <input className="app__submit" type="submit" value="Search" />
+              </form>
+            </div>
+            <div className="app__videos">
+              {videos.map(video => {
+                const value = video.id;
+                const url = `https://api.themoviedb.org/3/movie/${value}/videos?api_key=616093e66ab252685ad921e5c4680152`;
+                var videoDisplay;
+                fetch(url)
+                  .then(res => res.json())
+                  .then(data => (videoDisplay = data))
+                  .then(() => console.log(videoDisplay));
+                return (
+                  <div
+                    key={video.id}
+                    className="VideoTile"
+                    onClick={() => {
+                      {
+                        video.poster_path == null
+                          ? window.open(`https://www.youtube.com/results?search_query=${query}`)
+                          : window.open(`https://www.youtube.com/watch?v=${videoDisplay.results[0].key}`);
+                      }
+                    }}
+                  >
+                    <div className="row">
+                      {video.poster_path == null ? (
+                        <img
+                          className="videoTile__img"
+                          src={`https://c.tenor.com/0bN9L54PMmsAAAAC/coming-soon-see-it-soon.gif`}
+                          alt="card image"
+                          style={{ width: '100%', height: 360 }}
+                        />
+                      ) : (
+                        <img
+                          className="videoTile__img"
+                          src={`https://image.tmdb.org/t/p/w185${video.poster_path}`}
+                          alt="card image"
+                          style={{ width: '100%', height: 360 }}
+                        />
+                      )}
                     </div>
-                  );
-                })}
-              </div>
-            </Col>
-            <Alert color="light"></Alert>
-          </div>
-        ) : (
-          <div>
-            <Alert color="light">
-              <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
-
-              <Link to="/login" className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-              </Link>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
-                , you can try the default accounts:
-                <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-                <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-              </Translate>
-            </Alert>
-
-            <Alert color="light">
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
-              <Link to="/account/register" className="alert-link">
-                <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
-              </Link>
-            </Alert>
-          </div>
-        )}
+                  </div>
+                );
+              })}
+            </div>
+          </Col>
+          <Alert color="light"></Alert>
+        </div>
       </Col>
     </Row>
   );
