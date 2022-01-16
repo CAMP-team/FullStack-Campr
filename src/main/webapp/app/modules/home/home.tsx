4ff164dbda20f60c1b18473b1abc18a2277551c5
Carnell Poteat <nellp@mail.com>
2022-01-16 12:46:54 -0500

    Search function working again, working on trailers

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
@@ -8,54 +8,60 @@ import Axios from 'axios';
 import { useAppSelector } from 'app/config/store';
 import './home.scss';
 import './App.css';
-import VideoCard from './VideoCard';
 import Header from 'app/shared/layout/header/header';
+import MovieCard from './MovieCard';
 
 /* eslint-disable */
 
-function videoApp() {
+function App() {
   const API_URL = 'https://api.themoviedb.org/3';
-  const [videos, setVideos] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState('');
 
-  const fetchMovies = async () => {
+  const fetchMovies = async searchKey => {
+    const type = searchKey ? 'search' : 'discover';
     const {
       data: { results },
-    } = await Axios.get(`${API_URL}/discover/movie`, {
+    } = await Axios.get(`${API_URL}/${type}/movie`, {
       params: {
         api_key: '616093e66ab252685ad921e5c4680152',
+        query: searchKey,
       },
     });
-    setVideos(results);
-    console.log('data', results);
+
+    setMovies(results);
   };
 
   useEffect(() => {
-    fetchMovies();
+    fetchMovies(searchKey);
   }, []);
 
-  const renderVideos = () => videos.map(video => <VideoCard key={video.id} video={video} />);
+  const renderVideos = () => movies.map(movie => <MovieCard key={movie.id} video={movie} />);
+
+  const searchMovies = e => {
+    e.preventDefault();
+    fetchMovies(searchKey);
+  };
 
   const account = useAppSelector(state => state.authentication.account);
   return (
-    <Row>
-      <Col md="8" className="pad"></Col>
-      <Col md="9">
-        <div className="VideoApp">
-          <div className="container">{renderVideos()}</div>
-        </div>
-        {account?.login ? (
-          <div>
-            <Col md="5"></Col>
-          </div>
-        ) : (
-          <Alert color="light"></Alert>
-        )}
-        <div>
-          <div></div>
-        </div>
-      </Col>
-    </Row>
+    <>
+      <div className="MovieApp">
+        <header>
+          <h1>HelloYoutubue</h1>
+
+          <form onSubmit={searchMovies}>
+            <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchKey(e.target.value)} />
+            <button type={'submit'}>Search</button>
+          </form>
+        </header>
+        {/* {searchKey}    */} {/* //Test to check search function */}
+        <div className="container">{renderVideos()}</div>
+      </div>
+
+      <Col md="9">{account?.login ? <div></div> : <Alert color="light"></Alert>}</Col>
+    </>
   );
 }
 
-export default videoApp;
+export default App;
--- target/classes/static/app/administration.chunk.js
+++ target/classes/static/app/administration.chunk.js
@@ -20,7 +20,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "iframe {\n  background: white;\n}", "",{"version":3,"sources":["webpack://./src/main/webapp/app/modules/administration/docs/docs.scss"],"names":[],"mappings":"AAAA;EACE,iBAAA;AACF","sourcesContent":["iframe {\n  background: white;\n}\n"],"sourceRoot":""}]);
+___CSS_LOADER_EXPORT___.push([module.id, "iframe {\n  background: white;\n}", "",{"version":3,"sources":["webpack://./src/main/webapp/app/modules/administration/docs/docs.scss"],"names":[],"mappings":"AAAA;EACE,iBAAA;AACF","sourcesContent":["iframe {\r\n  background: white;\r\n}\r\n"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
