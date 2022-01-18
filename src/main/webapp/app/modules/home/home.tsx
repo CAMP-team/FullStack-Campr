d17bdff0311a35780ae837852ced693f2539f65a
Carnell Poteat <nellp@mail.com>
2022-01-18 18:36:15 -0500

    Updated layout css

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,26 +1,24 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 /* eslint-disable */
-import { Col, Alert } from 'reactstrap';
+import { Col, Alert, Card } from 'reactstrap';
 import { useAppSelector } from 'app/config/store';
 import './home.scss';
 import './App.css';
-import MovieCard from './MovieCard';
+import VideoCard from './VideoCard';
 import axios from 'axios';
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import Youtube from 'react-youtube';
-import YouTube from 'react-youtube';
-import ReactPlayer from 'react-player';
 
 function Home() {
   const IMAGE_PATH = 'https://image.tmdb.org/t/p/original';
   const API_URL = 'https://api.themoviedb.org/3';
-  const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState([]);
+  const [video, setVideo] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState([]);
   const [searchKey, setSearchKey] = useState('');
   // const [playTrailer,setPlayTrailer] = useState(false)
 
-  const fetchMovies = async searchKey => {
+  const getFirstVideoFetch = async searchKey => {
     const type = searchKey ? 'search' : 'discover';
     const {
       data: { results },
@@ -31,11 +29,11 @@ function Home() {
       },
     });
 
-    setSelectedMovie(results[0]);
-    setMovies(results);
+    setSelectedVideo(results[0]);
+    setVideo(results);
   };
 
-  const fetchMovie = async id => {
+  const getSecondVideoFetch = async id => {
     const { data } = await axios.get(`${API_URL}/movie/${id}`, {
       params: {
         api_key: '616093e66ab252685ad921e5c4680152',
@@ -44,62 +42,61 @@ function Home() {
     });
     return data;
   };
-  const selectmovie = async movie => {
-    const data = await fetchMovie(movie.id);
-    console.log(data);
-    setSelectedMovie(movie);
+  const selectvideo = async video => {
+    const data = await getSecondVideoFetch(video.id);
+    const trailer = data.videos.results.find(mov => mov.official === true);
+    // const key = data;
+    console.log(trailer);
+    setSelectedVideo(video);
   };
 
   useEffect(() => {
-    fetchMovies(searchKey);
+    getFirstVideoFetch(searchKey);
   }, []);
 
-  const renderPosters = () => movies.map(movie => <MovieCard key={movie.id} movie={movie} selectmovie={selectmovie} />);
+  const renderPosters = () => video.map(video => <VideoCard key={video.id} video={video} selectvideo={selectvideo} />);
   const account = useAppSelector(state => state.authentication.account);
-  const searchMovies = e => {
+  const searchVideos = e => {
     e.preventDefault();
-    fetchMovies(searchKey);
+    getFirstVideoFetch(searchKey);
   };
 
-  const renderTrailer = () => {
-    const trailer = selectedMovie.videos.results.find(video => video.name === 'Official Trailer');
+  const renderVideoTrailers = () => {
+    const data = getSecondVideoFetch(selectedVideo);
+
     console.log(trailer);
 
-    return (
-      <YouTube
-        videoId={trailer.key}
-        // containerClassName={"youtube-container"}
-        // opts={{
-        //   width: "100",
-        //   height: "100%"
-      />
-    );
+    return trailer;
+    <Youtube />;
   };
+
+  // Card component
+  // with buttons
 
   return (
     <>
-      <div className="MovieApp">
+      <div className="videoapp">
         <header className={'header'}>
           <div className={'header-content max-center'}>
-            <form onSubmit={searchMovies}>
+            <form onSubmit={searchVideos}>
               <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchKey(e.target.value)} />
               <button type={'submit'}>Search</button>
             </form>
           </div>
+          <div></div>
         </header>
-        <div className="imageHeader" style={{ backgroundImage: `url('${IMAGE_PATH}${selectedMovie.backdrop_path}')` }}>
+        <div className="imageHeader" style={{ backgroundImage: `url('${IMAGE_PATH}${selectedVideo.backdrop_path}')` }}>
           <div className="imageHeader-content max-center">
-            {selectedMovie.videos ? renderTrailer() : null}
             {/* <button className={"button"} onClick={() =>setPlayTrailer(true)} >Play Trailer</button> */}
-            <h1 className={'imageHeader-title'}>{selectedMovie.title}</h1>
-            {selectedMovie.overview ? <p className={'imageHeader-overview'}>{selectedMovie.overview}</p> : null}
+            <h1 className={'imageHeader-title'}>{selectedVideo.title}</h1>
+            {selectedVideo.overview ? <p className={'imageHeader-overview'}>{selectedVideo.overview}</p> : null}
           </div>
         </div>
 
-        <div className="container max-center">{renderPosters()}</div>
+        <div className="container">{renderPosters()}</div>
       </div>
 
-      <Col md="9">{account?.login ? <div></div> : <Alert color="light"></Alert>}</Col>
+      <Col md="1">{account?.login ? <div></div> : <Alert color="light"></Alert>}</Col>
     </>
   );
 }
